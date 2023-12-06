765b25f75c2f4a1a745a9f5231d7d27ccfe1d33d
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-12-05 22:31:43 -0500

    :sparkles: (S5C52) Implementado crear en el cliente.service.ts y en form.component.ts

--- src/app/clients/client.service.ts
+++ src/app/clients/client.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Client } from './client';
-import { Observable, map, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientService {
+  private urlEndPoint: string = 'http://localhost:8080/api/clients';
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-  private urlEndPonit: string = 'http://localhost:8080/api/clients';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getClients(): Observable<Client[]> {
-    return this.http.get<Client[]>(this.urlEndPonit).pipe(
-      map((rta) => rta as Client[])
-    );
+    return this.http
+      .get<Client[]>(this.urlEndPoint)
+      .pipe(map((rta) => rta as Client[]));
+  }
+
+  create(client: Client): Observable<Client> {
+    return this.http.post<Client>(this.urlEndPoint, client, {
+      headers: this.httpHeaders,
+    });
   }
 }
--- src/app/clients/form.component.ts
+++ src/app/clients/form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Client } from './client';
+import { ClientService } from './client.service';
 
 @Component({
   selector: 'app-form',
@@ -9,7 +11,14 @@ export class FormComponent {
   client: Client = new Client();
   title: string = 'Crear cliente';
 
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+  ) {}
+
   create(): void {
-    console.log('Client', this.client);
+    this.clientService.create(this.client).subscribe({
+      next: () => this.router.navigate(['/clients']),
+    });
   }
 }
