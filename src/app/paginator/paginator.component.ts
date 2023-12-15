3425704bb71aef81c265a2132dacc4e3d158b354
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-12-14 19:34:24 -0500

    :sparkles: (S11C92) Implementado rangos de páginas en el componente Paginador

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,16 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html',
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
   @Input() paginator: any;
   pages: number[];
+  since: number;
+  until: number;
 
-  ngOnInit(): void {
-    this.pages = new Array(this.paginator.totalPages)
-      .fill(0)
-      .map((_valor, indice) => indice + 1);
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    this.since = Math.min(
+      Math.max(1, this.paginator.number - 4),
+      this.paginator.totalPages - 5,
+    );
+    this.until = Math.max(
+      Math.min(this.paginator.totalPages, this.paginator.number + 4),
+      6,
+    );
+    if (this.paginator.totalPages > 5) {
+      this.pages = new Array(this.until - this.since + 1)
+        .fill(0)
+        .map((_value, index) => index + this.since);
+    } else {
+      this.pages = new Array(this.paginator.totalPages)
+        .fill(0)
+        .map((_value, index) => index + 1);
+    }
   }
 }
